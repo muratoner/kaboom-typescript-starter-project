375e2f5d94366a39d9aff39ef5297ffd772751e1
Murat Öner <muhackgames@hotmail.com>
2023-12-24 14:59:55 +0300

    first commit

---
+++ src/kaboomCtx.ts
@@ -0,0 +1,8 @@
+import kaboom from "kaboom";
+
+const k = kaboom({
+  width: 850,
+  height: 500,
+})
+
+export default k;
---
+++ src/main.ts
@@ -0,0 +1,50 @@
+import { Comp, GameObj } from "kaboom";
+import k from "./kaboomCtx";
+
+
+const titleComps : Array<Comp> = [
+  k.text("Hello World!", {font: "sinko"}),
+  k.pos(200,100),
+  k.scale(4),
+];
+
+function enableFullscreen() {
+  onKeyPress("f", () => {
+    fullscreen(!isFullscreen())
+  });
+}
+
+
+k.scene("mainScene", async () => {
+  enableFullscreen(); //fullscreen is scoped by scene. You need this if you want your players to be able to go fullscreen on this scene.
+  
+  k.add(titleComps);
+  
+  await loadBean(); // You can load assets within the scene using async/await
+  const bean : GameObj = k.add([
+    k.sprite("bean"),
+    k.pos(50, 200),
+  ])
+
+  k.add([
+    k.text("move with arrow keys", {font: "sinko"}),
+    k.pos(k.center()),
+    k.origin("center"),
+    k.scale(2),
+  ])
+
+  k.add([
+    k.text("Press f to go Fullscreen", {font: "sinko"}),
+    k.pos(10,20),
+    k.scale(2),
+  ])
+
+  const SPEED = 500;
+  k.onKeyDown("left", () => bean.move(-SPEED,0));
+  k.onKeyDown("right", () => bean.move(SPEED, 0));
+  k.onKeyDown("up", () => bean.move(0, -SPEED));
+  k.onKeyDown("down", () => bean.move(0, SPEED));
+});
+
+
+k.go("mainScene");
